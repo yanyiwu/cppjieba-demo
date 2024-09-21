95dd245b4d6a4e826535fa669bb00a162ce334d2
yanyiwu <wuyanyi09@foxmail.com>
2024-09-21 16:03:54 +0800

    fix dictpath

--- demo.cpp
+++ demo.cpp
@@ -2,11 +2,11 @@
 
 using namespace std;
 
-const char* const DICT_PATH = "../dict/jieba.dict.utf8";
-const char* const HMM_PATH = "../dict/hmm_model.utf8";
-const char* const USER_DICT_PATH = "../dict/user.dict.utf8";
-const char* const IDF_PATH = "../dict/idf.utf8";
-const char* const STOP_WORD_PATH = "../dict/stop_words.utf8";
+const char* const DICT_PATH = "../cppjieba/dict/jieba.dict.utf8";
+const char* const HMM_PATH = "../cppjieba/dict/hmm_model.utf8";
+const char* const USER_DICT_PATH = "../cppjieba/dict/user.dict.utf8";
+const char* const IDF_PATH = "../cppjieba/dict/idf.utf8";
+const char* const STOP_WORD_PATH = "../cppjieba/dict/stop_words.utf8";
 
 int main(int argc, char** argv) {
   cppjieba::Jieba jieba(DICT_PATH,
